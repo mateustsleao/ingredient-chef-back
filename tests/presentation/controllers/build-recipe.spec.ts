7779fcbfb9179c337ff70e9aa24cc4586ce87218
Mateus Toledo da Silveira Leão <89942457+mateustsleao@users.noreply.github.com>
2023-07-20 07:35:35 +0100

    test: ensure calls BuildRecipeController with correct values

---
+++ tests/presentation/controllers/build-recipe.spec.ts
@@ -0,0 +1,64 @@
+import { BuildRecipeController } from '../../../src/controller/build-recipe-controller'
+import { type RecipeModel } from '../../../src/domain/models/recipe'
+import { type BuildRecipe, type BuildRecipeModel } from '../../../src/domain/use-cases/build-recipe'
+import { type HttpRequest } from '../protocols/http'
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    name: 'valid_name',
+    ingredients: [{
+      description: 'valid_description'
+    }],
+    meal: 'valid_meal',
+    quantity: 1
+  }
+})
+
+const makeBuildRecipe = (): BuildRecipe => {
+  class BuildRecipeStub implements BuildRecipe {
+    build (recipe: BuildRecipeModel): RecipeModel {
+      return {
+        id: 'valid_id',
+        name: 'valid_name',
+        ingredients: [{
+          description: 'valid_description'
+        }],
+        steps: [{
+          step: 'valid_step'
+        }],
+        nutrition: {
+          values: [{
+            nutriente: 'valid_nutriente'
+          }],
+          fact: 'valid_nutritionFact'
+        },
+        portions: 1,
+        time: 1
+      }
+    }
+  }
+  return new BuildRecipeStub()
+}
+
+type SutTypes = {
+  sut: BuildRecipeController
+  buildRecipeStub: BuildRecipe
+}
+
+const makeSut = (): SutTypes => {
+  const buildRecipeStub = makeBuildRecipe()
+  const sut = new BuildRecipeController(buildRecipeStub)
+  return {
+    sut,
+    buildRecipeStub
+  }
+}
+describe('Build Recipe Controller', () => {
+  test('Should call BuildRecipe with correct values', () => {
+    const { sut, buildRecipeStub } = makeSut()
+    const buildRecipeSpy = jest.spyOn(buildRecipeStub, 'build')
+    const httpRequest = makeFakeRequest()
+    sut.handle(httpRequest)
+    expect(buildRecipeSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+})
